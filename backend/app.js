805fe6498dbc289de69bd1ea3c6a1f27f7195eda
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-02-18 16:18:48 +0300

    _0

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb')
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
   .then(() => console.log('connected'))
   .catch((e) => console.log(e));
 
