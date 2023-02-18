60f25c88f3c816dd89bc426d2197a2fe31f30e0f
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-02-18 21:13:20 +0300

    _0

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -26,6 +26,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: 'none',
+          secure: true,
         })
         .send({ data: user });
     })
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -10,12 +10,14 @@ module.exports = (req, res, next) => {
     return next(new UnauthorizedError('Необходима авторизация. Не передан токен'));
   }
 
-  const payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', (err, decoded) => {
-    if (err) {
-      return next(new UnauthorizedError('Необходима авторизация. Ошибка проверки токена'));
-    }
-    return decoded;
-  });
+  let payload;
+
+  try {
+    // верифицируем токен
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    return next(new UnauthorizedError('Необходима авторизация. Ошибка проверки токена'));
+  }
 
   req.user = payload; // записываем пейлоуд в объект запроса
 
