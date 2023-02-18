c7f5625d4eaab6fa6208da7fa86615073c8ee843
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-02-18 21:41:47 +0300

    _0

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -17,7 +17,7 @@ const cors = function (req, res, next) {
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Credentials', 'true');
   }
 
   // Если это предварительный запрос, добавляем нужные заголовки
