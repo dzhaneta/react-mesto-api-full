8d7fd3d406487b5045ced61f9fa83dc7df702603
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-02-18 20:54:14 +0300

    _0

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -25,7 +25,7 @@ module.exports.login = (req, res, next) => {
           domain: 'localhost',
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: true,
+          sameSite: 'none',
         })
         .send({ data: user });
     })
