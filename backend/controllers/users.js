b3a4e0435960e527cb69c00579a9dd7dc214d954
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-02-18 20:33:35 +0300

    _0

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -22,6 +22,7 @@ module.exports.login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           // token - наш JWT токен, который мы отправляем
+          domain: 'localhost',
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: true,
