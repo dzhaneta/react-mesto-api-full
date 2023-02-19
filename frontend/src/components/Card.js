db30506b5cd22a941b1d06d1dafbd13f456ab157
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-02-19 20:30:57 +0300

    _0

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,7 +3,7 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Card({ card, onClick, onCardLike, onCardDelete }) {
   const currentUser = React.useContext(CurrentUserContext);
-  const isOwn = card.owner === currentUser._id;
+  const isOwn = (card.owner._id === currentUser._id);
 
   const isLiked = card.likes.some((like) => like === currentUser._id);
   const cardLikeButtonClassName = `photo-grid__like-button ${
