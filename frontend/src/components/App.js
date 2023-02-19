803f86bf27c1b02077bcd68c63d7bd45b3fd16e1
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-02-19 19:59:43 +0300

    _0

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -171,7 +171,7 @@ function App() {
   }
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((like) => like._id === currentUser._id);
     api
       .changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
