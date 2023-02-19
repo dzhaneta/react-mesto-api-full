cc0165f2f0916086cc2bed69d11e24d45f68b0a6
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-02-19 16:48:23 +0300

    _0

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -44,7 +44,7 @@ function App() {
         .getUserInfo()
         .then((profile) => {
           handleLoggedIn();
-          setCurrentUser(profile.data);
+          setCurrentUser(profile);
         })
         .catch((err) => {
           console.log(err);
