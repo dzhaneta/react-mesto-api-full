a943ce1ebe0a4f53f2141ffcc5b0a498e368fdc7
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-02-19 19:51:49 +0300

    _0

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,9 +5,7 @@ function Card({ card, onClick, onCardLike, onCardDelete }) {
   const currentUser = React.useContext(CurrentUserContext);
   const isOwn = card.owner._id === currentUser._id;
 
-  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
-  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const isLiked = card.likes.some((i) => i === currentUser._id);
   const cardLikeButtonClassName = `photo-grid__like-button ${
     isLiked ? "photo-grid__like-button_active" : ""
   }`;
